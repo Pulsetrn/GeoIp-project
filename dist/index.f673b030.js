c50697a067ae7f523cae50ac6f7685663898c15c
Ковалев Егор <pulseefast@gmail.com>
2024-06-28 18:39:10 +0700

    project

---
+++ dist/index.f673b030.js
@@ -0,0 +1,78 @@
+const btn = document.querySelector("button");
+const idInput = document.querySelector(".search-bar__input");
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+let myMap = undefined;
+btn.addEventListener("click", getData);
+idInput.addEventListener("keydown", handleKey);
+async function getData() {
+    inputData = idInput.value;
+    if (validateIp(inputData)) {
+        const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_hctYzT6UtwqXjbNkZPgsO4HIXWteR&ipAddress=${inputData}`);
+        const data = await response.json();
+        setInfo(data);
+    }
+}
+function handleKey(event) {
+    if (event.key == "Enter") getData();
+}
+function setInfo(data) {
+    const { lat, lng, city } = data.location;
+    ipInfo.innerText = data.ip;
+    locationInfo.innerText = data.location.country + " " + data.location.region;
+    timezoneInfo.innerText = data.location.timezone;
+    ispInfo.innerText = data.isp;
+    if (myMap !== undefined) {
+        console.log(myMap.geoObjects);
+        myMap.setCenter([
+            lat,
+            lng
+        ]);
+        createPlacemark(myMap, lat, lng);
+    } else {
+        ymaps.ready(init());
+        myMap.setCenter([
+            lat,
+            lng
+        ]);
+        createPlacemark(myMap, city, lat, lng);
+    }
+}
+function validateIp(ip) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)) return true;
+    alert("Input ip-adress invalid");
+    return false;
+}
+function init() {
+    // Создание карты.
+    myMap = new ymaps.Map("map", {
+        center: [
+            37.64,
+            55.76
+        ],
+        zoom: 10,
+        type: "yandex#satellite",
+        // Карта будет создана без
+        // элементов управления.
+        controls: []
+    });
+}
+function createPlacemark(map, city, lat, lng) {
+    let placemark = new ymaps.Placemark([
+        lat,
+        lng
+    ], {
+        iconContent: city
+    }, {
+        preset: "islands#yellowStretchyIcon",
+        // Отключаем кнопку закрытия балуна.
+        balloonCloseButton: false,
+        // Балун будем открывать и закрывать кликом по иконке метки.
+        hideIconOnBalloonOpen: false
+    });
+    map.geoObjects.add(placemark);
+}
+
+//# sourceMappingURL=index.f673b030.js.map
